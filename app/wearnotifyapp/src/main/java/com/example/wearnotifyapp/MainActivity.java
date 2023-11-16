2cc223c5467d9d1a1a40e30fda390dc8e492fd06
Yin ThuAye <yin.thu.aye0@gmail.com>
2023-11-16 02:07:52 -0500

    Initial commit

---
+++ app/wearnotifyapp/src/main/java/com/example/wearnotifyapp/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.wearnotifyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import android.annotation.SuppressLint;
+import android.widget.Toast;
+
+import com.example.wearnotifyapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String CHANNEL_ID = "CHANNEL_01";
+    //private static final int NOTIFICATION_PERMISSION_REQUEST_CODE = 1;
+    ActivityMainBinding mainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
+        init();
+    }
+
+    private void init() {
+        createNotificationChannel();
+        mainBinding.btnNotify.setOnClickListener(this);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //
+            String channelName = getString(R.string.channel_name);
+            String channelDescription = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            channel.setDescription(channelDescription);
+            //
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == mainBinding.btnNotify.getId()) {
+            String notificationTitle = getString(R.string.notification_title);
+            String notificationText = getString(R.string.notification_text);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+            //
+            builder.setContentTitle(notificationTitle);
+            builder.setContentText(notificationText);
+            builder.setSmallIcon(R.drawable.ic_notification);
+            //
+            NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                // Requesting the permission
+                ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.POST_NOTIFICATIONS }, 0);
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+                // You need to request notification permission.
+
+                return;
+            }else{
+                // Permission has already been granted, post the notification
+                managerCompat.notify(1, builder.build());
+            }
+            managerCompat.notify(1, builder.build());
+        }
+
+    }
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 0) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission was granted, you can post the notification now
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setContentTitle(getString(R.string.notification_title))
+                        .setContentText(getString(R.string.notification_text))
+                        .setSmallIcon(R.drawable.ic_notification);
+                managerCompat.notify(1, builder.build());
+            } else {
+                // Permission denied, boo! Disable the functionality that depends on this permission.
+                // You can show a toast or alert dialog here to inform the user that the permission was not granted.
+                Toast.makeText(this, "Notification permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
